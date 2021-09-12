6c583df7ce991c49048c9d721a429bbb376122f8
Siamion Makarski <s.makarski.s@gmail.com>
2021-09-13 00:22:38 +0200

    Initial `list` cmd implementation - default to today

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"time"
 
+	"google.golang.org/api/calendar/v3"
+
 	"github.com/makarski/gcaler/config"
 	gcal "github.com/makarski/gcaler/google/calendar"
 	"github.com/makarski/gcaler/staff"
@@ -16,19 +18,51 @@ import (
 
 var out = os.Stdout
 
-func List(_ gcal.GCalendar, _ *config.Template) error {
-	fmt.Println("not implemented")
+func List(gCalendar gcal.GCalendar, template *config.Template) error {
+	ctx := context.Background()
+	calSrv, tz, err := calSrvLocation(ctx, &gCalendar, template)
+	if err != nil {
+		return err
+	}
+
+	now := time.Now()
+	start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, tz)
+	end := start.Add(time.Hour * 24)
+
+	events, err := calSrv.Events.
+		List(template.CalID).
+		TimeMin(start.Format(time.RFC3339)).
+		TimeMax(end.Format(time.RFC3339)).
+		Do()
+
+	if err != nil {
+		return err
+	}
+
+	for _, event := range events.Items {
+		startEnd := make([]string, 0, 2)
+		for _, dateTime := range []*calendar.EventDateTime{event.Start, event.End} {
+			parsed, err := time.Parse(time.RFC3339, dateTime.DateTime)
+			if err != nil {
+				return err
+			}
+			startEnd = append(startEnd, parsed.Format(time.Kitchen))
+		}
+
+		fmt.Printf("  * %s - %s: %s (%s)\n",
+			startEnd[0],
+			startEnd[1],
+			event.Summary,
+			event.Status,
+		)
+	}
+
 	return nil
 }
 
 func Plan(gCalendar gcal.GCalendar, template *config.Template) error {
 	ctx := context.Background()
-	calSrv, err := gCalendar.CalendarService(ctx, handleAuthConsent)
-	if err != nil {
-		return err
-	}
-
-	tz, err := time.LoadLocation(template.Timezone)
+	calSrv, tz, err := calSrvLocation(ctx, &gCalendar, template)
 	if err != nil {
 		return err
 	}
@@ -69,6 +103,24 @@ func Plan(gCalendar gcal.GCalendar, template *config.Template) error {
 
 	_, err = io.Copy(out, summary)
 	return err
+}
+
+func calSrvLocation(
+	ctx context.Context,
+	gCalendar *gcal.GCalendar,
+	template *config.Template,
+) (*calendar.Service, *time.Location, error) {
+	calSrv, err := gCalendar.CalendarService(ctx, handleAuthConsent)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	tz, err := time.LoadLocation(template.Timezone)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return calSrv, tz, nil
 }
 
 func summaryTxtBuffer(countAssgnmts int) *bytes.Buffer {
